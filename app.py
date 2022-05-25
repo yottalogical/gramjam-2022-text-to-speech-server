fd78ba14b10cbf6f9d92a9c65285f65ee33576a8
yottalogical <email@yottalogical.com>
2022-05-25 16:48:53 -0400

    Implement the server using gTTS

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import flask
+import gtts
+import io
 
 app = flask.Flask(__name__)
 
@@ -7,4 +9,13 @@ app = flask.Flask(__name__)
 def index():
     text = flask.request.get_data(as_text=True)
 
-    return text
+    return text_to_speech(text)
+
+
+def text_to_speech(text: str) -> bytes:
+    audio = io.BytesIO()
+
+    tts = gtts.gTTS(text)
+    tts.write_to_fp(audio)
+
+    return audio.getvalue()
